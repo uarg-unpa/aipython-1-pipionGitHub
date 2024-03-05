2f62e0181e0869f6ee213c6156f0b654604fba48
Agustina Maldonado <161722590+pipionGitHub@users.noreply.github.com>
2024-03-05 18:46:53 +0000

    alknadsl

--- entrega/guia/principal.py
+++ entrega/guia/principal.py
@@ -1,3 +1,36 @@
+#print("Probando funcion"+" print()")
+#print("print()"*3)
+#print(10,3.14,"hola",False)
+
+#edad = int(input("Ingrese edad: "))
+#print("su edad es:",edad)
+#print("su edad es: "+str(edad))
+#print(f"edad: {edad}")
+
+#num1 = 3
+#num2 = 8
+#print(f"{num1} + {num2} = {num1 + num2}")
+#print(f"{num1} - {num2} = {num1 - num2}")
+#print(f"{num1} * {num2} = {num1 * num2}")
+
+ # 
+
+texto = "kdjS hFaD SDd sFGha"
+#title
+print(texto.title())
+print(texto)
+texto = texto.title()
+print(texto)
+
+#upper lower
+print(texto.upper())
+print(texto.lower())
+
+#replace
+print(texto.replace(" ", "<3"))
+
+
+
 
 
 
